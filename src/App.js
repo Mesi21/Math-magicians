080fef5fa01025e25ac10414687ab5900c6da6a1
Molnar Emese <mse212002@gmail.com>
2021-11-30 16:55:18 +0200

    :recycle: refactored code to use class-based components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import { Component } from 'react';
 import Calculator from './components/Calculator';
 
-const App = () => (
-  <div id="app">
-    <Calculator />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
 
 export default App;
